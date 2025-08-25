71f8262ddd9b185a883bd71e658b166e244f88d6
pehlivanyunuscan <pehlivanyunuscan@hotmail.com>
2025-08-25 13:49:29 +0300

    feat: GetSampleEnv fonksiyonu eklendi

---
+++ endpoints/sample_env.go
@@ -0,0 +1,43 @@
+package endpoints
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+type SampleEnvResponse struct {
+	Description      string `json:"description"`
+	SampleEnvContent string `json:"sample_env_content"`
+	Status           string `json:"status"`
+}
+
+func GetSampleEnv(apiUrl string) (*SampleEnvResponse, error) {
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", apiUrl+"/sample-env", nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create request: %w", err)
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("failed to send request: %w", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read response body: %w", err)
+	}
+
+	var sampleEnvResp SampleEnvResponse
+	if err := json.Unmarshal(body, &sampleEnvResp); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal response: %w", err)
+	}
+	return &sampleEnvResp, nil
+}
